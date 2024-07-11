1fe296c64f428b2ecd5476195fdcd8d0c3aa3c4f
Jacob B Thayyil <jacobbt07@gmail.com>
2024-07-11 18:16:33 +0530

    SubHeader completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import SubHeader from './Components/SubHeader/SubHeader';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-    
+      <SubHeader/>
     </div>
   );
 }
---
+++ src/Components/SubHeader/SubHeader.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './SubHeader.css'
+const SubHeader = () => {
+  const [dropdown,setDropdown] = useState(false)
+  const primeDropdown = () =>{
+    setDropdown(!dropdown)
+  }
+  return (
+    <div className='sub-header'>
+        <div>
+          <div className="three-line"></div>
+          <div>All</div>
+        </div>
+        <div>
+          Fresh
+        </div>
+        <div>Amazon miniTV</div>
+        <div>Sell</div>
+        <div>Best Sellers</div>
+        <div>Mobiles</div>
+        <div>Today's Deals</div>
+        <div>Customer Service</div>
+        <div className='prime' onMouseEnter={primeDropdown} onMouseLeave={primeDropdown}>
+          <span>Prime</span>
+          <i class="fa-solid fa-caret-down"></i>
+          {
+            dropdown&&(
+              <div className="prime-content">
+                <div className="triangle-prime"></div>
+                <img src="https://m.media-amazon.com/images/G/31/prime/NavFlyout/TryPrime/2018/Apr/IN-Prime-PIN-TryPrime-MultiBen-Apr18-400x400._CB612889281_.jpg" alt="Prime Flyout" />
+              </div>
+            )
+          }
+        </div>
+       
+        <div>Fashion</div>
+        <div className="big-savings">
+          <img src="https://m.media-amazon.com/images/G/31/Events/img24/PD24/GW/PD24_SWM_Dates_V2._CB569503763_.jpg" alt="Big Savings" />
+        </div>
+        
+    </div>
+  )
+}
+
+export default SubHeader
